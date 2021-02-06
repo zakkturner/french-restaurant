281723b92de0e355a3214106a002cef2a2260f37
Zach Turner <zakkturner@yahoo.com>
2021-02-05 21:31:30 -0500

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
