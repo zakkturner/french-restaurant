945f54d1e71b3bb3d4b8817af9a5cafeb0cd756a
zakkturner <30674356+zakkturner@users.noreply.github.com>
2021-02-06 22:52:13 -0500

    menu toggle out draft done

--- src/app/mobile-menu/mobile-menu.component.ts
+++ src/app/mobile-menu/mobile-menu.component.ts
@@ -11,6 +11,9 @@ export class MobileMenuComponent implements OnInit {
   constructor() {}
   @ViewChild('line1') line1Ref: ElementRef;
   @ViewChild('line2') line2Ref: ElementRef;
+  @ViewChild('overlay') overlay: ElementRef;
+  @ViewChild('menu') menu: ElementRef;
+  @ViewChild('grid') grid: ElementRef;
   links = [
     {
       title: 'Home',
@@ -48,15 +51,50 @@ export class MobileMenuComponent implements OnInit {
         ease: 'power3.inOut',
       },
       'lines'
-    ).to(
-      this.line2Ref.nativeElement,
-      {
-        duration: 0.3,
-        drawSVG: '50% 50%',
-        ease: 'power3.inOut',
-      },
-      'lines'
-    );
+    )
+      .to(
+        this.line2Ref.nativeElement,
+        {
+          duration: 0.3,
+          drawSVG: '50% 50%',
+          ease: 'power3.inOut',
+        },
+        'lines'
+      )
+      .to(this.grid.nativeElement, { duration: 0.2, opacity: 0 })
+      .to(
+        this.overlay.nativeElement,
+        {
+          duration: 0.5,
+          x: '100%',
+          ease: 'circ.inOut',
+          css: {
+            borderRadius: '50% 0 0 50%',
+            right: '0',
+            width: '0',
+            height: '0',
+          },
+        },
+        'menu'
+      )
+      .to(
+        this.menu.nativeElement,
+        {
+          duration: 0.5,
+          x: '100%',
+          ease: 'circ.inOut',
+
+          css: {
+            borderRadius: '0 0 0 50%',
+            right: '0',
+            width: '0',
+            height: '0',
+          },
+          delay: 0.1,
+        },
+        'menu'
+      )
+      .to(this.menu.nativeElement, { css: { display: 'none' } });
     tl.play();
   }
 
